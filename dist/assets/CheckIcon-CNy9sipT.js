67741c2c9af5926dbfcb79f14036c5ec1d002e8f
Ismoil_azamjonov <azamjonov07ismoil@gmail.com>
2026-04-30 19:07:48 +0500

    gg

---
+++ dist/assets/CheckIcon-CNy9sipT.js
@@ -0,0 +1,1 @@
+import{D as e,P as t,w as n}from"./index-BBPyHoX7.js";function r(r,i){return t(),e(`svg`,{xmlns:`http://www.w3.org/2000/svg`,fill:`none`,viewBox:`0 0 24 24`,"stroke-width":`1.5`,stroke:`currentColor`,"aria-hidden":`true`,"data-slot":`icon`},[n(`path`,{"stroke-linecap":`round`,"stroke-linejoin":`round`,d:`m4.5 12.75 6 6 9-13.5`})])}export{r as t};
